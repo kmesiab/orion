e51fa5e84f08e3b9fb8c5c17105935e23fd6648b
Frankie <frankidatank@gmail.com>
2024-01-22 19:23:15 -0700

    feat/add analysis page display

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,61 @@
-import {useState} from 'react';
-import './App.scss';
-import {ProcessPullRequest} from "../wailsjs/go/main/App.js";
+import { useState } from "react";
+import "./App.scss";
+import { ProcessPullRequest } from "../wailsjs/go/main/App.js";
+import { GetCodeReviewFromAPI } from "../wailsjs/go/main/App.js";
 import DiffViewer from "./components/DiffViewer/DiffViewer.jsx";
+import AnalysisViewer from "./components/AnalysisViewer/AnalysisViewer.jsx";
+import Stack from "@mui/material/Stack";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
 
 function App() {
-    const [resultText, setResultText] = useState('');
-    const [url, setUrl] = useState('');
+  const [pullRequestText, setPullRequestText] = useState("");
+  const [codeReviewText, setCodeReviewText] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [url, setUrl] = useState("");
 
-    const updateName = (e) => setUrl(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+  const updateUrl = (e) => setUrl(e.target.value);
+  const processPullRequestText = (result) => {
+    setPullRequestText(result);
+    //Once we have pull request text we can call the API to get the code review text
+    setIsLoading(true);
+    GetCodeReviewFromAPI(result).then(proccessCodeReview);
+  };
+  const proccessCodeReview = (result) => {
+    setCodeReviewText(result);
+    setIsLoading(false);
+  };
 
-    function processPullRequest() {
-        ProcessPullRequest(url).then(updateResultText);
-    }
+  function processPullRequest() {
+    ProcessPullRequest(url).then(processPullRequestText);
+  }
 
-    return (
-        <div id="App">
-            <div id="input" className="input-box">
-                <input id="url" className="input" onChange={updateName} name="input" type="text" placeholder={"https://github.com/owner/repo/pull/1234"}/>
-                <button className="btn" onClick={processPullRequest}>Review</button>
-            </div>
-            <DiffViewer text={resultText} />
-        </div>
-    )
+  return (
+    <div id="App">
+      <div id="input" className="input-box">
+        <input
+          id="url"
+          className="input"
+          onChange={updateUrl}
+          name="input"
+          type="text"
+          placeholder={"https://github.com/owner/repo/pull/1234"}
+        />
+        <button className="btn" onClick={processPullRequest}>
+          Review
+        </button>
+      </div>
+      <Stack spacing={2}>
+        <ListItem>
+          <AnalysisViewer loading={isLoading} analysisReport={codeReviewText} />
+        </ListItem>
+        {codeReviewText && <Divider />}
+        <ListItem>
+          <DiffViewer text={pullRequestText} />
+        </ListItem>
+      </Stack>
+    </div>
+  );
 }
 
-export default App
+export default App;
