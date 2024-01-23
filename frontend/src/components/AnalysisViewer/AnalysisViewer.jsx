4aa7a6c2c095239c37fab3d4f8435cf14afaf8b0
Frankie <frankidatank@gmail.com>
2024-01-22 19:23:15 -0700

    feat/add analysis page display

---
+++ frontend/src/components/AnalysisViewer/AnalysisViewer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Markdown from "react-markdown";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+
+const AnalysisViewer = ({ loading, analysisReport }) => {
+  if (loading) {
+    return <LoadingSpinner text="Generating your report... 🤖" />;
+  }
+  if (!analysisReport) {
+    return null;
+  }
+  const analysisStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  };
+
+  return (
+    <Box sx={analysisStyles}>
+      <Typography variant="h4" gutterBottom>
+        Analysis Report
+      </Typography>
+      <Markdown>{analysisReport}</Markdown>
+    </Box>
+  );
+};
+
+export default AnalysisViewer;
